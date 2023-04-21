a5ae3bc81fc46e3d46509f3a92c5cb599e1ecff5
Allrivenjs <jaimeruizxdd2@gmail.com>
2023-04-21 20:51:32 +0000

    finished

--- main.go
+++ main.go
@@ -1,7 +1,197 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func textToPolynomial(text string) ([]int, error) {
+	re := regexp.MustCompile(`x\^(\d+)`)
+	//get x alone
+	reX := regexp.MustCompile(`x`)
+
+	matches := re.FindAllStringSubmatch(text, -1)
+
+	var polynomial []int
+	for _, match := range matches {
+		exp, err := strconv.Atoi(match[1])
+		if err != nil {
+			return nil, fmt.Errorf("error al analizar el exponente: %v", err)
+		}
+		polynomial = append(polynomial, exp)
+	}
+	if reX != nil {
+		polynomial = append(polynomial, 1)
+	}
+
+	return polynomial, nil
+}
+func polynomialDegree(polynomial []int) int {
+	maxDegree := 0
+	for _, exp := range polynomial {
+		if exp > maxDegree {
+			maxDegree = exp
+		}
+	}
+	return maxDegree
+}
+func polynomialToBinary(polynomial []int) string {
+	maxExp := 0
+	for _, exp := range polynomial {
+		if exp > maxExp {
+			maxExp = exp
+		}
+	}
+
+	bitLength := maxExp + 1
+	binaryRepresentation := make([]byte, bitLength)
+
+	for i := 0; i < bitLength; i++ {
+		binaryRepresentation[i] = '0'
+	}
+
+	for _, exp := range polynomial {
+		binaryRepresentation[bitLength-exp-1] = '1'
+	}
+
+	return string(binaryRepresentation)
+}
+func binaryToPolynomial(binaryRepresentation string) []int {
+	var polynomial []int
+	bitLength := len(binaryRepresentation)
+
+	for i := 0; i < bitLength; i++ {
+		if binaryRepresentation[i] == '1' {
+			exp := bitLength - i - 1
+			polynomial = append(polynomial, exp)
+		}
+	}
+
+	return polynomial
+}
+func generateCRCTable(poly uint32) [256]uint32 {
+	var table [256]uint32
+	for i := uint32(0); i < 256; i++ {
+		c := i
+		for j := 0; j < 8; j++ {
+			if c&1 == 1 {
+				c = (c >> 1) ^ poly
+			} else {
+				c >>= 1
+			}
+		}
+		table[i] = c
+	}
+	return table
+}
+func crc32(binData string, poly uint32) (uint32, error) {
+	data, err := strconv.ParseUint(binData, 2, 64)
+	if err != nil {
+		return 0, fmt.Errorf("error al convertir la cadena binaria a número: %v", err)
+	}
+
+	table := generateCRCTable(poly)
+	crc := uint32(0xFFFFFFFF)
+
+	logrus.Infof("Procesando datos: %s", binData)
+	for i := 0; i < len(binData); i++ {
+		logrus.Infof("Iteración %d, CRC actual: %08X", i, crc)
+		crc = (crc >> 8) ^ table[byte(crc)^byte(data&0xFF)]
+		data >>= 8
+	}
+
+	checksum := ^crc
+	logrus.Infof("Checksum CRC-32 calculado: %08X", checksum)
+
+	return checksum, nil
+}
+
+func xor(a, b string) string {
+	result := ""
+	length := len(a)
+	if len(b) < length {
+		length = len(b)
+	}
+
+	for i := 0; i < length; i++ {
+		if a[i] == b[i] {
+			result += "0"
+		} else {
+			result += "1"
+		}
+	}
+	return result
+}
+func generateZerosWithGradPoly(i int) string {
+	var result string
+	for _i := 0; _i < i; _i++ {
+		result += fmt.Sprintf("%d ", 0)
+	}
+	return result
+}
+
+func crc32Binary(data, poly string, grad int) string {
+	logrus.Info("Iniciando el cálculo del CRC-32 en binario")
+	// Añadir ceros al final de los datos igual al grado del polinomio
+	data += "00000000000000000000000000000000"
+	logrus.Infof("Datos con ceros añadidos: %s", data)
+
+	divisor := poly
+	remainder := data[:33]
+
+	for i := 33; i < len(data); i++ {
+		if remainder[0] == '1' {
+			logrus.Infof("Iteración %d: Dividiendo %s por %s", i-32, remainder, divisor)
+			remainder = xor(remainder, divisor)
+		}
+		remainder = remainder[1:] + string(data[i])
+		logrus.Infof("Iteración %d: Residuo actual: %s", i-32, remainder)
+	}
+
+	if remainder[0] == '1' {
+		logrus.Infof("Última iteración: Dividiendo %s por %s", remainder, divisor)
+		remainder = xor(remainder, divisor)
+	}
+	remainder = remainder[1:]
+
+	return remainder
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Ingrese el polinomio a codificar: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		logrus.Fatal("Ocurrio un error al leer el polinomio", err)
+		panic(err)
+	}
+
+	text = text[:len(text)-1]
+	logrus.Println("El polinomio es: ", text)
+
+	polynomial, err := textToPolynomial(text)
+	if err != nil {
+		logrus.Fatal("Ocurrio un error al convertir el polinomio", err)
+		panic(err)
+	}
+
+	logrus.Println("El polinomio es: ", polynomial)
+	grad := polynomialDegree(polynomial)
+	logrus.Println("El grado del polinomio es: ", grad)
+
+	binPolynomial := polynomialToBinary(polynomial)
+	logrus.Println("El polinomio en binario es: ", binPolynomial)
+
+	//polynomial = binaryToPolynomial(binPolynomial)
+	//logrus.Println("El polinomio en binario es: ", binPolynomial)
+	//poly := uint32(0xEDB88320)
+	poly := "1001"
+	checksum := crc32Binary(binPolynomial, poly, grad)
+	logrus.Infof("Checksum CRC-32: %s\n", checksum)
+	//x^6 + x^5 + x^4 + x^3 + x^2 + x
 }
